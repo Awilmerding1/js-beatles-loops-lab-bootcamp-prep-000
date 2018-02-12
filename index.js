31ccddb91e53ed75777d07db8dcd2fc7373b9f57
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-12 19:01:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ for (var i = 0; i < musicians.length; i++) {
  return array;
  }
  
-function johnLennonFacts(array) {
+function johnLennonFacts(facts) {
   const facts = [ "He was the last Beatle to learn to drive",
         "He was never a vegetarian",
         "He was a choir boy and boy scout",
