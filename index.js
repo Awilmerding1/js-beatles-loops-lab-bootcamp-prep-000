64465d64c2523c555949c2bef81094088440cd1f
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-12 18:37:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
   var empty = [];
   for(var i = 0; i < musicians.length; i++);
- { empty.push(musicians[i] + " plays " + instruments[i]);
+ { 
+  empty.push("musicians[i] + " plays " + instruments[i]");
  }
   return empty;
 }
