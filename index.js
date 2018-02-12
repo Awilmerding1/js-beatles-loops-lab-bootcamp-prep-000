3109ef29f9f6f79b43c7783a3aacf5e2c7606b6c
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-02-12 19:09:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ for (var i = 0; i < musicians.length; i++) {
 function johnLennonFacts(facts) {
   var i = 0;
   while(i < facts.length) {
-    facts[i] += "!!!"
+    facts[i] += "!!!";
+    i++;
   }
   return facts;
 }
